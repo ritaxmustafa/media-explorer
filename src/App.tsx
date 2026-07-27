57d1bbb3e39e03acf9defe79a46d0077a07f5941
ritaxmustafa <ritaxmustafa@gmail.com>
2026-07-27 22:54:12 +0200

    chore: initialize project with development tooling

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-background">
+      <div className="container mx-auto px-4">
+        <h1>Video Browser</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
